c07956af16d8d703f4a3684c5eeca3681a0f2c15
MICROWA7E <168425659+MICROWA7E@users.noreply.github.com>
2025-01-31 17:49:15 +0000

    Server Files

---
+++ [Clothing]/genericambulanceessentialsmf/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_genericambulanceessentialsmf.meta',
+  'mp_f_freemode_01_mp_f_genericambulanceessentialsmf.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_genericambulanceessentialsmf.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_genericambulanceessentialsmf.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
+dependency '/assetpacks'
